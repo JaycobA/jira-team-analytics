a8897e1dc8578a042a160fe2dc2b2f31148d7f66
Jakob Anders <jakob.anders@immobilienscout24.de>
2018-07-24 16:06:35 +0200

    extract Issue

---
+++ issue.js
@@ -0,0 +1,80 @@
+class Issue {
+  constructor(key, fields, changelog) {
+    this.key = key;
+    this.fields = fields;
+    this.changelog = changelog;
+    this.statusChanges = this.getStatusChanges();
+    this.workPeriods = this.getWorkPeriods();
+    this.timeInProgress = this.getTimeInProgress();
+    this.storyPoints = this.fields.customfield_10006 || 1;
+    this.workPerDayInProgress = this.storyPoints / this.timeInProgress;
+  }
+
+  static fromApiResult(apiIssue) {
+    return new Issue(apiIssue.key, apiIssue.fields, apiIssue.changelog);
+  }
+
+  getStatusChanges() {
+    return _.chain(this.changelog.histories)
+      .filter(this.historyWasInProgress)
+      .map(this.statusChangesOnly)
+      .map((statusChange) => {
+        return {
+          date: statusChange.created,
+          from: statusChange.items[0].fromString,
+          to: statusChange.items[0].toString
+        };
+      })
+      .value();
+  }
+
+  getWorkPeriods() {
+    let workPeriod;
+    return _.reduce(this.statusChanges, (periods, statusChange) => {
+      let date = moment(statusChange.date);
+
+      if(statusChange.to == 'In Progress') {
+        workPeriod = { start: date.startOf('day') };
+      } else {
+        workPeriod.end = date.startOf('day');
+        periods.push(workPeriod);
+      }
+      return periods;
+    }, []);
+  }
+
+  getTimeInProgress() {
+    let workPeriodStart = null;
+    return _.reduce(this.workPeriods, (sum, workPeriod) => {
+      sum += this.workdayCount(workPeriod.start, workPeriod.end);
+      return sum;
+    }, 0);
+  }
+
+  historyWasInProgress(history) {
+    return _.chain(history.items)
+      .map((item) => {
+        return item.field == 'status' && (item.fromString == 'In Progress' || item.toString == 'In Progress');
+      }).some().value();
+  }
+
+  statusChangesOnly(statusChange) {
+    statusChange.items = _.filter(statusChange.items, (item) => {
+      return item.field == 'status';
+    });
+    return statusChange;
+  }
+
+  workdayCount(start, end) {
+    var startDateMoment = moment(start);
+    var endDateMoment = moment(end);
+    var days = Math.round(-startDateMoment.diff(endDateMoment, 'days') + startDateMoment.diff(endDateMoment, 'days') / 7 * 2);
+    if (endDateMoment.day() === 6) {
+      days--;
+    }
+    if (startDateMoment.day() === 0) {
+      days--;
+    }
+    return days + 1;
+  }
+}
--- popup.js
+++ popup.js
@@ -13,36 +13,15 @@ Config.withInferredConfig((config) => {
     }
   });
 
-  function workdayCount(start, end) {
-    var startDateMoment = moment(start);
-    var endDateMoment = moment(end);
-    var days = Math.round(-startDateMoment.diff(endDateMoment, 'days') + startDateMoment.diff(endDateMoment, 'days') / 7 * 2);
-    if (endDateMoment.day() === 6) {
-      days--;
-    }
-    if (startDateMoment.day() === 0) {
-      days--;
-    }
-    return days + 1;
-  }
-
   let jiraClient = new JiraClient(API_URL);
   let jql = `${config.get("filterQuery")} AND issuetype not in subTaskIssueTypes() and resolutiondate > -6w order by resolutiondate desc`;
 
   jiraClient.search(jql).then((issues) => {
     let workDone = _.chain(issues)
       .map((issue) => {
-        return _.pick(issue, ['key', 'fields', 'changelog']);
-      }).map((issue) => {
-        issue.statusChanges = getStatusChanges(issue);
-        issue.workPeriods = getWorkPeriods(issue);
-        issue.timeInProgress = getTimeInProgress(issue);
-        return issue;
+        return Issue.fromApiResult(issue);
       }).filter((issue) => {
         return issue.timeInProgress > 0;
-      }).each((issue) => {
-        issue.storyPoints = issue.fields.customfield_10006 || 1;
-        issue.workPerDayInProgress = issue.storyPoints / issue.timeInProgress;
       }).value();
 
     let periodEnd = moment();
@@ -79,53 +58,4 @@ Config.withInferredConfig((config) => {
 
     console.log(workDonePerWeek);
   });
-
-  function historyWasInProgress(history) {
-    return _.chain(history.items)
-      .map((item) => {
-        return item.field == 'status' && (item.fromString == 'In Progress' || item.toString == 'In Progress');
-      }).some().value();
-  }
-
-  function statusChangesOnly(statusChange) {
-    statusChange.items = _.filter(statusChange.items, (item) => {
-      return item.field == 'status';
-    });
-    return statusChange;
-  }
-
-  function getStatusChanges(issue) {
-    return _.chain(issue.changelog.histories)
-      .filter(historyWasInProgress)
-      .map(statusChangesOnly)
-      .map((statusChange) => {
-        return {
-          date: statusChange.created,
-          from: statusChange.items[0].fromString,
-          to: statusChange.items[0].toString
-        };
-      })
-      .value();
-  }
-
-  function getWorkPeriods(issue) {
-    return _.reduce(issue.statusChanges, (periods, statusChange) => {
-      date = moment(statusChange.date);
-      if(statusChange.to == 'In Progress') {
-        workPeriod = { start: date.startOf('day') };
-      } else {
-        workPeriod.end = date.startOf('day');
-        periods.push(workPeriod);
-      }
-      return periods;
-    }, []);
-  }
-
-  function getTimeInProgress(issue) {
-    let workPeriodStart = null;
-    return _.reduce(issue.workPeriods, (sum, workPeriod) => {
-      sum += workdayCount(workPeriod.start, workPeriod.end);
-      return sum;
-    }, 0);
-  }
 });
